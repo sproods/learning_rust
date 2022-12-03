8a9e5506a0e899cf244ed3f0acf8c375ecec4fb8
sproods <106177602+sproods@users.noreply.github.com>
2022-12-02 22:05:11 -0500

    multiples cambios hechos, sin yo saber nada de lo que está ocurriendo, por Cargo.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -7,8 +7,7 @@ fn main(){
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
 }
